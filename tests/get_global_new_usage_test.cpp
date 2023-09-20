3b6d7bff2ee9874ddc4231b8dfcf07e976db6787
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-09-19 21:30:18 -0600

    more idiomatic branch

--- tests/get_global_new_usage_test.cpp
+++ tests/get_global_new_usage_test.cpp
@@ -18,7 +18,7 @@ int main(){
 
     std::size_t end_usage = safemem::get_global_new_usage();
 
-    std::cout << "Starting usage: " << starting_usage << "\nMid usage: " << mid_usage << "\nEnd usage: " << end_usage << "\nYou " << (starting_usage != end_usage ? "have a memory leak" : "are all good") << std::endl;
+    std::cout << "Starting usage: " << starting_usage << "\nMid usage: " << mid_usage << "\nEnd usage: " << end_usage << "\nYou " << ((end_usage - starting_usage) ? "are all good" : "have a memory leak") << std::endl;
 
     return EXIT_SUCCESS;
 }
